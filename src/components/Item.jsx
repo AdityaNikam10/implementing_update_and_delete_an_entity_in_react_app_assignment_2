92f13220de7a2f2d5fcb6a12d8ca924f69e3dedd
Aditya Nikam <aditya.nikam.s73@kalvium.community>
2025-04-08 09:43:32 +0530

    done

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,8 +1,22 @@
-const Item = ({ item }) => {
+import React from "react";
+import PropTypes from "prop-types";
 
-    // Render a single item
-    // Add a Delete and Edit button
-    return null;
+const Item = ({ item, deleteItem }) => {
+  return (
+    <div>
+      <p>{item.name}</p> {/* Assuming the item has a 'name' property */}
+      <button onClick={() => deleteItem(item.id)}>Delete</button>
+    </div>
+  );
+};
+
+// PropTypes validation
+Item.propTypes = {
+  item: PropTypes.shape({
+    id: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
+    name: PropTypes.string.isRequired,
+  }).isRequired,
+  deleteItem: PropTypes.func.isRequired,
 };
 
 export default Item;
--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,14 +1,26 @@
+import React from "react";
+import PropTypes from "prop-types";
 import Item from "./Item";
 
-const ItemList = ({ items }) => {
-    // your code here
-    return (
-        <>
-            {items.map((item) => (
-                <Item key={item.id} item={item} />
-            ))}
-        </>
-    );
+const ItemList = ({ items, deleteItem }) => {
+  return (
+    <>
+      {items.map((item) => (
+        <Item key={item.id} item={item} deleteItem={deleteItem} />
+      ))}
+    </>
+  );
+};
+
+//PropTypes validation
+ItemList.propTypes = {
+  items: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
+      name: PropTypes.string.isRequired,
+    })
+  ).isRequired,
+  deleteItem: PropTypes.func.isRequired,
 };
 
 export default ItemList;
